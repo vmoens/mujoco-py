ad2fe5a41893d43c3871a256ecc70b54e6ebecb0
vmoens <vincentmoens@gmail.com>
2023-04-26 12:56:23 +0100

    amend

--- build.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-def build(setup_kwargs):
-    os.environ['MUJOCO_PY_FORCE_REBUILD'] = 'True'
-    os.environ['MUJOCO_PY_SKIP_ACTIVATE'] = 'True'
-    import mujoco_py  # noqa: force build
